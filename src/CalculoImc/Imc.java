b09a21f58a8b494f53953af3beaf11f9a867977b
Aluno <Aluno@SN-279228>
2019-02-05 20:30:59 -0200

    Mudanças básicas e isso

--- src/CalculoImc/Imc.java
+++ src/CalculoImc/Imc.java
@@ -7,7 +7,7 @@ public class Imc {
      * 
      * Calcula o IMC
      */
-    
+    // e isso
     public double calcular( double peso, double altura){
         double fim = 0;
         fim = peso / (altura * altura);
